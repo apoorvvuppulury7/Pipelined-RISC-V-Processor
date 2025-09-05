c3038604baa971bb2cad8cb0bfab5a466750f78f
Apoorv Vuppulury <139952588+apoorvvuppulury7@users.noreply.github.com>
2025-09-05 16:50:30 +0530

    Update ALU.v

--- Single Cycle/ALU.v
+++ Single Cycle/ALU.v
@@ -1,26 +1,30 @@
 //The ALU is the part of a CPU that performs arithmetic and logical operations.
 //This Verilog module implements a simple 32-bit ALU with support for a few operations and a Zero flag.
 
-module ALU (ALU_Cntrl, In1, In2, Zero, ALU_Result);
-	input wire [3:0] ALU_Cntrl;
-	input wire [31:0] In1, In2;
-	output reg [31:0] ALU_Result;
-	output wire Zero;
+module ALU #(parameter WIDTH = 32) (
+    input  wire [3:0] ALU_Cntrl,
+    input  wire [WIDTH-1:0] In1, In2,
+    output reg  [WIDTH-1:0] ALU_Result,
+    output wire Zero
+);
 	
-	localparam [3:0] AND = 4'b0000, OR = 4'b0001, ADD = 4'b0010, SUB = 4'b0110;
-	
+	localparam [3:0] AND = 4'b0000, OR  = 4'b0001, ADD = 4'b0010, SUB = 4'b0110, SLT = 4'b0111, XOR = 4'b0011, NOR = 4'b1100;
 	always @(*)
 		begin
 		case(ALU_Cntrl)
-			AND: ALU_Result = In1 & In2;
-			OR: ALU_Result = In1 | In2;
-			ADD: ALU_Result	= In1 + In2;
-			SUB: ALU_Result = In1 - In2;
-			default: ALU_Result = 32'bx;
-		endcase
-		end
-	
-	assign Zero = (ALU_Result == 32'b0);
+            AND: ALU_Result = In1 & In2;
+            OR : ALU_Result = In1 | In2;
+            ADD: ALU_Result = In1 + In2;
+            SUB: ALU_Result = In1 - In2;
+            XOR: ALU_Result = In1 ^ In2;
+            NOR: ALU_Result = ~(In1 | In2);
+            SLT: ALU_Result = ($signed(In1) < $signed(In2)) ? 32'b1 : 32'b0;
+            default: ALU_Result = {WIDTH{1'b0}};
+        endcase
+    end
 
+	assign Zero = (ALU_Result == {WIDTH{1'b0}});
+	
 endmodule
+
 
