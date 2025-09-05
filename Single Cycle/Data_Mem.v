93d7383b9ed24985b8d932a680105fea978f675d
Apoorv Vuppulury <139952588+apoorvvuppulury7@users.noreply.github.com>
2025-09-05 17:00:00 +0530

    Update Data_Mem.v

--- Single Cycle/Data_Mem.v
+++ Single Cycle/Data_Mem.v
@@ -1,10 +1,15 @@
 //This module represents the data memory block in your CPU, where loads (lw) and stores (sw) happen.
 //It supports 1) 32-bit word read, 2) 32-bit word write, 3) Reset initialization
 
-module Data_mem(addr,wr_data,rd_data,mem_read,mem_write, clk, rst);
-	input wire [31:0] addr, wr_data;
-	input wire mem_read, mem_write,clk, rst;
-	output wire [31:0] rd_data;
+module Data_mem (
+    input  wire [31:0] addr,      
+    input  wire [31:0] wr_data,  
+    input  wire        mem_read,  
+    input  wire        mem_write, 
+    input  wire        clk,      
+    input  wire        rst,       
+    output wire [31:0] rd_data    
+);
 	
 	reg [7:0] d_mem [(2^32)-1:0];
 	integer i;
@@ -30,4 +35,5 @@ module Data_mem(addr,wr_data,rd_data,mem_read,mem_write, clk, rst);
 	
 	assign rd_data = (mem_read)? {d_mem[addr+3],d_mem[addr+2],d_mem[addr+1], d_mem[addr]} : 32'bx ;
 endmodule
+
 
